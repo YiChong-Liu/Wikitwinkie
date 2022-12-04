426498dc4c05998fc1e36089d2f2217197020e3c
Neil Gupta <nogupta@umass.edu>
2022-12-04 17:03:20 -0500

    implement login

--- sessions/index.ts
+++ sessions/index.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "crypto";
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
@@ -10,6 +11,9 @@ import { NLPRoute } from "./utils/utils.js";
 const PORT = 4001;
 
 const app = express();
+
+// session expiry in milliseconds
+const SESSION_EXPIRY_MS = 24 * 3600 * 1000
 
 // const ajv = new Ajv();
 
@@ -42,22 +46,43 @@ const db = redis.createClient({
 app.post("/login", NLPRoute({
   bodyScehma: {
     properties: {
-      username: {type: "string"},
-      password: {type: "string"}
+      username: { type: "string" },
+      password: { type: "string" }
     }
   },
   sessionCookieRequired: true
 } as const, async (req, res) => {
   // todo: move this route in accountmanagement to a different port that is not publicly exposed
-  await axios.post("http://accountmanagement:4001/checkpassword", );
-  res.status(200).send("todo").end();
+  const checkPassResponse = await axios.post("http://accountmanagement:4001/checkpassword", {
+    username: req.body.username,
+    password: req.body.password
+  });
+  if (checkPassResponse.data.success) {
+
+    // create a new session
+    const sessionId = randomUUID();
+    await db.set(sessionId, JSON.stringify({
+      username: req.body.username,
+      expiry: new Date(Date.now() + SESSION_EXPIRY_MS).toISOString()
+    }))
+
+    res.status(200).send({
+      success: true,
+      sessionId: sessionId
+    });
+  } else {
+    res.status(200).send({
+      success: false,
+      error: "Invalid username/password"
+    });
+  }
 }));
 
 app.post("/validate", NLPRoute({
   bodyScehma: {
     properties: {
-      sessionId: {type: "string"},
-      username: {type: "string"}
+      sessionId: { type: "string" },
+      username: { type: "string" }
     }
   }
 } as const, async (req, res) => {
