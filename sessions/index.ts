333c734a30bf87224ee034eb56a3502673e8ba00
Neil Gupta <nogupta@umass.edu>
2022-12-04 16:08:23 -0500

    NLPRoute

--- sessions/index.ts
+++ sessions/index.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 import redis from "redis";
 // import Ajv from "ajv/dist/jtd.js";
 // import type { JTDSchemaType, JTDDataType }  from "ajv/dist/jtd";
-import { asyncRoute, asyncRouteWithBody } from "./utils/utils.js";
+import { NLPRoute } from "./utils/utils.js";
 
 const PORT = 4001;
 
@@ -39,28 +39,35 @@ const db = redis.createClient({
   }
 });
 
-app.post("/login", asyncRouteWithBody({
-  properties: {
-    username: {type: "string"},
-    password: {type: "string"}
-  }
+app.post("/login", NLPRoute({
+  bodyScehma: {
+    properties: {
+      username: {type: "string"},
+      password: {type: "string"}
+    }
+  },
+  sessionCookieRequired: true
 } as const, async (req, res) => {
   // todo: move this route in accountmanagement to a different port that is not publicly exposed
   await axios.post("http://accountmanagement:4001/checkpassword", );
   res.status(200).send("todo").end();
 }));
 
-app.post("/validate", asyncRouteWithBody({
-  properties: {
-    sessionId: {type: "string"},
-    username: {type: "string"}
+app.post("/validate", NLPRoute({
+  bodyScehma: {
+    properties: {
+      sessionId: {type: "string"},
+      username: {type: "string"}
+    }
   }
 } as const, async (req, res) => {
   // todo
   res.status(200).send("todo").end();
 }));
 
-app.post("/logout", asyncRoute(async (req, res) => {
+app.post("/logout", NLPRoute({
+  sessionCookieRequired: true
+}, async (req, res) => {
   // todo
   res.status(200).send("todo").end();
 }));
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,5 +1,3 @@
-// import { fileURLToPath } from "url";
-// import path from "path";
 import type { Request, Response } from "express";
 import Ajv from "ajv/dist/jtd.js";
 import type { JTDSchemaType, JTDDataType } from "ajv/dist/jtd";
@@ -7,40 +5,32 @@ import type * as expressCore from "express-serve-static-core";
 
 const ajv = new Ajv();
 
-// export const projectRoot = path.dirname(path.dirname(fileURLToPath(import.meta.url)));
-
-export const asyncRoute = (route: (req: Request, res: Response) => Promise<void>) =>
-                          (req: Request, res: Response, next = console.error) =>
-                          Promise.resolve(route(req, res)).catch(next);
-
-export const asyncRouteWithBody = <T>(bodySchema: T, route: (req: Request<expressCore.ParamsDictionary, any,
-                                                                          JTDDataType<T>>,
-                                                             res: Response) => Promise<void>) => {
-  const validateSchema = ajv.compile(bodySchema as JTDSchemaType<JTDDataType<T>>);
+export const NLPRoute = <T>(
+  config: {
+    sessionCookieRequired?: boolean,
+    bodyScehma?: T
+  },
+  routeHandler: (req: Request<expressCore.ParamsDictionary, any, JTDDataType<T>>,
+                 res: Response) => Promise<void>
+) => {
+  const validateSchema = config.bodyScehma === undefined ? null
+                         : ajv.compile(config.bodyScehma as JTDSchemaType<JTDDataType<T>>);
   return (request: Request, response: Response, next=console.error) => {
-    if (validateSchema(request.body)) {
-      Promise.resolve(route(request, response)).catch(next);
-    } else {
-      response.status(400);
-      console.log(`Error in request body: ${validateSchema.errors}`)
-      // response.send(`Error in body: ${error}\n`);
-      // // send automatically ends the response
+    if (validateSchema !== null) {
+      if (!validateSchema(request.body)) {
+        response.status(400);
+        console.log(`Error in request body: ${validateSchema.errors}`)
+        // response.send(`Error in body: ${error}\n`);
+        // // send automatically ends the response
+        return;
+      }
     }
+    if (config.sessionCookieRequired) {
+      // TODO: handle session cookies
+    }
+    Promise.resolve(routeHandler(request, response)).catch(next);
   };
 };
-
-// const requireAttrs = term => (obj, attrs, response) => {
-//   for (const attr of attrs) {
-//     if (!obj.hasOwnProperty(attr)) {
-//       response.status(400);
-//       response.send(`Missing required ${term}: ${attr}`);
-//       // send automatically ends the response
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-// export const requireParams = requireAttrs("parameter");
 
 // export const serve404 = response =>
 //   response.status(404).sendFile(`${projectRoot}/client/404.html`, {
