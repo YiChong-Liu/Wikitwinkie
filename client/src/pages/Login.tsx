41642d4adcb65934d9d4fef500bfabe2866cd620
Neil Gupta <nogupta@umass.edu>
2022-12-06 14:42:10 -0500

    client linking

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,7 +3,7 @@ import App from "../lib/App";
 import React from "react";
 
 const Login = () => {
-  return <App>This is text on the login page</App>;
+  return <App title="Log In">This is text on the login page</App>;
 }
 
 export default Login;
