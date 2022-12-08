7e4b83c80f975c7cb69dd5f66d10a0203ee5090d
Neil Gupta <nogupta@umass.edu>
2022-12-08 15:57:39 -0500

    more interfaces

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -1,13 +1,21 @@
+
+// sessions
 export interface SesssionsLoginResponse {
   success: boolean;
 }
-
 export interface SesssionsLoginResponseSuccessful extends SesssionsLoginResponse {
   success: true,
   sessionId: string,
 }
-
 export interface SesssionsLoginResponseFailed extends SesssionsLoginResponse {
   success: false,
   error: string,
+}
+export interface SessionsValidateSessionResponse {
+  sessionValid: boolean;
+}
+
+// accountmanagement
+export interface AccountManagementCheckPasswordResponse {
+  success: boolean;
 }
