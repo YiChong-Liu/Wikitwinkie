b427748f555de659bfd0078a4b01a36ddcad1aff
Neil Gupta <nogupta@umass.edu>
2022-12-09 15:54:29 -0500

    fix merge issue

--- utils/interfaces.ts
+++ utils/interfaces.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export interface SessionsLoginResponse {
   success: boolean;
 }
@@ -7,20 +6,7 @@ export interface SessionsLoginResponseSuccessful extends SessionsLoginResponse {
   success: true,
   sessionId: string,
 }
-
 export interface SessionsLoginResponseFailed extends SessionsLoginResponse {
-=======
-
-// sessions
-export interface SesssionsLoginResponse {
-  success: boolean;
-}
-export interface SesssionsLoginResponseSuccessful extends SesssionsLoginResponse {
-  success: true,
-  sessionId: string,
-}
-export interface SesssionsLoginResponseFailed extends SesssionsLoginResponse {
->>>>>>> 44a31677858062b1b6f8bfd4487dbb5a99e3a1cb
   success: false,
   error: string,
 }
