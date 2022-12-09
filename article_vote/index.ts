2ee288a904a2ad08d67561b33c77f72e3a10ac09
Yichong Liu <yichongliu23@gmail.com>
2022-12-09 16:35:37 -0500

    update client/README.md

--- article_vote/index.ts
+++ article_vote/index.ts
@@ -2,3 +2,30 @@ import redis from "redis"
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import { NLPRoute } from "./utils/utils.js";
+
+
+const app = express();
+const PORT = 4004;
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cors());
+
+const db = redis.createClient({
+    socket: {
+        host: "articlevotedb",
+        port: 6379
+    }
+})
+
+app.put("/articleVote", NLPRoute({
+    bodySchema: {
+        properties: {
+            articleId: { type: "string" },
+            vote: { type: "integer" }
+        }
+    },
+} as const, async (req, res) => {
+
+}))
