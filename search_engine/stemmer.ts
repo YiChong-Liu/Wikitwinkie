156b117defa7e02ffdb6acf99925d3a83b796339
Keith Pham <minhnghia2208@gmail.com>
2022-12-10 15:22:39 -0500

    add stemmer & stopword

---
+++ search_engine/stemmer.ts
@@ -0,0 +1,31 @@
+import { punc, stopword } from "./stopword";
+
+export class Stemmer {
+    input: string;
+    constructor(input: string) {
+        this.input = input;
+
+    }
+    private stopWord(content: string): string[] {
+        const arr: string[] = content.split(" ");
+        return arr.filter(x => !stopword.has(x));
+    }
+    
+    private punctuation(content: string): string {
+        const arr = content.split("");
+        const res = arr.filter(x => !punc.has(x));
+        return res.join("");
+    }
+    
+    // Porter Stemmer
+    private stemmer(content: string[]): string[] {
+        // TODO: Implement Porter Stemmer
+        return content
+    }
+
+    public stemming(): string[] {
+        const rmvStopWord = this.stopWord(this.input);
+        const rmvPunc = rmvStopWord.map(x => this.punctuation(x));
+        return rmvPunc;
+    }
+}
