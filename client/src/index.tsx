6866dc53104a2647a2513928abe09bfcc71a52b1
Keith Pham <minhnghia2208@gmail.com>
2022-12-11 17:49:08 -0500

    add bootstrap css

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<React.StrictMode>
