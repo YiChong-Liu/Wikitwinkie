2caa36c02ed6e15a1245d4c226416d14fbe3b621
Keith Pham <minhnghia2208@gmail.com>
2022-12-11 18:33:35 -0500

    client: search bar & search result

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,36 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import type { AxiosResponse } from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import NLPPage from "../lib/NLPPage";
+import { useState } from 'react';
+import SearchResult from "./SearchResult";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [ArticleId,setArticleId]=useState({articleId: []});
+
+  const onSearch = async () => {
+    const content: string = (document.getElementById("search-bar") as HTMLInputElement).value;
+    const response = await axios.post(
+      `http://${window.location.hostname}:4405/search/${content}`,
+      {withCredentials: true} // send and/or set cookies
+    );
+    
+    setArticleId(response.data);    
+  }
+
   return <NLPPage title="Placeholder Title">
     This is text on the home page
     <br/>
     <Link to="/login">Log in</Link>
+    <SearchResult result={ArticleId}></SearchResult>
+    <div className="input-group mb-3">
+      <input type="text" className="form-control" id="search-bar" placeholder="Search here..."/>
+      <div className="input-group-append">
+        <button type="button" className="btn btn-outline-secondary" onClick={onSearch}>Search</button>
+      </div>
+    </div>
+
   </NLPPage>;
 }
 
---
+++ client/src/pages/SearchResult.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import type { AxiosResponse } from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import NLPPage from "../lib/NLPPage";
+
+const SearchResult = (props: any) => {
+  const navigate = useNavigate();
+  const arr: string[] = props.result.articleId;
+
+  const search_result = document.getElementById("search-result");
+  const prev = document.getElementById("inner-search");
+  prev?.remove();
+
+  const inner = document.createElement("div");
+  inner.setAttribute("id", "inner-search");
+  for (let id of arr) {
+    // TODO: Retrieve article name by id
+    if (!document.getElementById(id)) {
+        const newdiv = document.createElement("div");
+        newdiv.setAttribute("id", id);
+        const newid = document.createTextNode(id);
+    
+        newdiv.appendChild(newid);
+        inner?.appendChild(newdiv);
+        search_result?.appendChild(inner);
+    }
+  }
+
+  return <div id="search-result">
+    
+  </div>;
+}
+
+export default SearchResult;
