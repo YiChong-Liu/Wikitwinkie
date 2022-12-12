da6c86faaaf3475acd8c2659760a172bd3537490
Neil Gupta <nogupta@umass.edu>
2022-12-12 11:00:22 -0500

    page create page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import CreatePage from "./pages/CreatePage";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
@@ -14,6 +16,7 @@ root.render(<React.StrictMode>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/createPage" element={<CreatePage/>}/>
     </Routes>
   </Router>
 </React.StrictMode>);
