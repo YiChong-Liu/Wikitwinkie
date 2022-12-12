b2f913a08f12d00fa029c62999e7aabcfe903ca7
Yichong Liu <yichongliu23@gmail.com>
2022-12-12 15:40:21 -0500

    change the name of homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -18,8 +18,8 @@ const Home = () => {
     setArticleId(response.data);
   }
 
-  return <NLPPage title="Placeholder Title">
-    This is text on the home page
+  return <NLPPage title="WikiTwinkie">
+    Please enter your search query in the search bar below:
     <br/>
     <SearchResult result={ArticleId}></SearchResult>
     <div className="input-group mb-3">
