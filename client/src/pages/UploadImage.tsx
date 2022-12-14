76e0ea1a5cc3cc58698232bd07898a5b3c137e99
Yichong Liu <yichongliu23@gmail.com>
2022-12-13 20:59:43 -0500

    debug image client

--- client/src/pages/UploadImage.tsx
+++ client/src/pages/UploadImage.tsx
@@ -14,46 +14,54 @@ const UploadImage = () => {
     // const navigate = useNavigate();
 
     const uploadImageSubmit = async () => {
+        const reader = new FileReader();
 
-        const image = (document.getElementById("image") as HTMLInputElement)?.files?[0]:null;
+        const image = (document.getElementById("image") as HTMLInputElement).files;
+        if (image === null) {
+            return;
+        }
+
+        reader.readAsBinaryString(image[0]);
         const imageName = (document.getElementById("imageName") as HTMLInputElement).value;
         const imageDescription = (document.getElementById("imageDescription") as HTMLInputElement).value;
 
         try {
-            const response = await axios.post(
-                `http://${window.location.hostname}:4003/image/${imageName}`,
-                {
-                    image: image,
-                    imageName: imageName,
-                    imageDescription: imageDescription
-                },
-                {withCredentials: true} // send and/or set cookies
-            )
-        }catch(e) {
+            reader.onload = async () => {
+                const response = await axios.post(
+                    `http://${window.location.hostname}:4003/image/${imageName}`,
+                    {
+                        image: reader.result,
+                        imageName: imageName,
+                        imageDescription: imageDescription
+                    },
+                    { withCredentials: true } // send and/or set cookies}
+                )
+            }
+        } catch (e) {
             console.log(e);
         }
     };
 
     return <NLPPage title="Upload Image">
-    <form>
-        <label className="labels" htmlFor="user">Image: </label>
-        <br/>
-        <input type="file" id="image" placeholder="Upload Image"/>
-        <br/>
-        <br/>
-        <label className="labels" htmlFor="user">Image Name: </label>
-        <br/>
-        <input type="text" id="imageName" placeholder="Enter Image Name"/>
-        <br/>
-        <br/>
-        <label className="labels" htmlFor="user">Image Description: </label>
-        <br/>
-        <input type="text" id="imageDescription" placeholder="Enter Image Description"/>
-        <br/>
-        <br/>
-        <br/>
-        <input className="button" type="button" value="Submit" onClick={uploadImageSubmit}/>
-    </form>
+        <form>
+            <label className="labels" htmlFor="user">Image: </label>
+            <br />
+            <input type="file" id="image" placeholder="Upload Image" />
+            <br />
+            <br />
+            <label className="labels" htmlFor="user">Image Name: </label>
+            <br />
+            <input type="text" id="imageName" placeholder="Enter Image Name" />
+            <br />
+            <br />
+            <label className="labels" htmlFor="user">Image Description: </label>
+            <br />
+            <input type="text" id="imageDescription" placeholder="Enter Image Description" />
+            <br />
+            <br />
+            <br />
+            <input className="button" type="button" value="Submit" onClick={uploadImageSubmit} />
+        </form>
     </NLPPage>;
 }
 
