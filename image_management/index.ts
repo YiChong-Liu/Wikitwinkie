7124ae7af8dd1e8998e9f1fd2e0eb6090a29029d
Yichong Liu <yichongliu23@gmail.com>
2022-12-13 21:47:30 -0500

    Make the image fully working

--- image_management/index.ts
+++ image_management/index.ts
@@ -15,10 +15,9 @@ const PORT = 4003;
 const EVENT_LISTENERS: EventType[] = [];
 
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
-app.use(bodyParser({ limit: "50mb" }));
 
 const db = redis.createClient({
     socket: {
